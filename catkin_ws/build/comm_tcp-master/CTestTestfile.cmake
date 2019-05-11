f4abcf326a24ee16a6a6f504757af271de122191
irsyadgan <irsyadgan@yahoo.com>
2019-05-11 10:53:25 +0700

    Ros

--- catkin_ws/build/comm_tcp-master/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/irsyad/catkin_ws/src/comm_tcp-master
-# Build directory: /home/irsyad/catkin_ws/build/comm_tcp-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/ros_service_assignment/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/irsyad/catkin_ws/src/ros_service_assignment/include".split(';') if "/home/irsyad/catkin_ws/src/ros_service_assignment/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lros_service_assignment".split(';') if "-lros_service_assignment" != "" else []
-PROJECT_NAME = "ros_service_assignment"
-PROJECT_SPACE_DIR = "/home/irsyad/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
